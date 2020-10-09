0160836dbcdb96993b2a459f5b79c0d7ec4e4939
mhombach <martin.hombach@gmail.com>
2020-10-09 15:10:21 +0200

    wip: switched to library mode

--- .storybook/helper.js
+++
@@ -1,6 +0,0 @@
-export const hideProperties = (component, ...properties) => {
-    component.argTypes = {};
-    for (const property of properties) {
-        component.argTypes[property] = { table: { disable: true } };
-    }
-}
---
+++ projects/smsdigital/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of smsdigital
+ */
+
+export * from './lib/components/button/button.component';
+export * from './lib/smsdigital.module';
