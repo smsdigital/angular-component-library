9a6b7c65ac65728a30d03312ecb13398db5e51cc
mhombach <martin.hombach@gmail.com>
2021-02-11 16:32:07 +0100

    added exports for button-group, checkbox and radiobutton-group

--- projects/smsdigital/src/public-api.ts
+++ projects/smsdigital/src/public-api.ts
@@ -3,4 +3,7 @@
  */
 
 export * from './lib/components/button/button.component';
+export * from './lib/components/button-group/button-group.component';
+export * from './lib/components/checkbox/checkbox.component';
+export * from './lib/components/radiobutton-group/radiobutton-group.component';
 export * from './lib/smsdigital.module';
